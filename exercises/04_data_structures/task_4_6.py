471090af21ccb30a8e08d992c15a8ca8e95d2e65
Ni_Os <nostroukh@yahoo.com>
2022-02-04 21:24:21 +0100

    Task 6 is solved

--- exercises/04_data_structures/task_4_6.py
+++ exercises/04_data_structures/task_4_6.py
@@ -18,3 +18,10 @@ Outbound Interface    FastEthernet0/0
 """
 
 ospf_route = "      10.0.24.0/24 [110/41] via 10.0.13.3, 3d18h, FastEthernet0/0"
+ospf_route = ospf_route.split(" ")
+ospf_route1 = ospf_route[6::]
+ospf_route2 = ospf_route1[1][1:6]
+ospf_route1[1] = ospf_route2
+ospf_route1.remove('via')
+output = 'Prefix{:>27}\nAD/Metric{:>17}\nNext-Hop{:>23}\nLast update{:>16}\nOutbound Interface{:>18}\n'
+print(output.format(ospf_route1[0],ospf_route1[1],ospf_route1[2],ospf_route1[3],ospf_route1[4]))
