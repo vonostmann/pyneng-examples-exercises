3322abcaa55978fef2ff3a5b8acb897eb76eba2e
vonostmann <nostroukh@yahoo.com>
2022-03-23 14:06:49 +0100

    Task is solved

--- exercises/06_control_structures/task_6_2b.py
+++ exercises/06_control_structures/task_6_2b.py
@@ -13,34 +13,40 @@
 Ограничение: Все задания надо выполнять используя только пройденные темы.
 """
 ip = input ("Введите IP устройства: ")
-try:
-   ip_spl = ip.split(".")
-   #делаем строку из ip:
-   ip_int = ip_spl[0] + ip_spl[1] + ip_spl[2] +ip_spl[3] 
-   #проверка на присутствие букв в адресе:
-   int((ip_spl[0]),10)/int((ip_spl[1]),10)/int((ip_spl[2]),10)/int((ip_spl[3]),10) 
-   #проверка на наличие разделителя "."
-   if '.' in ip:
-      #проверка на значение октетов меньше 255
-      if int((ip_spl[0]),10) <= 255 and int((ip_spl[1]),10) <= 255 and int((ip_spl[2]),10) <= 255 and int((ip_spl[3]),10) <= 255: 
-         #ip_int = ip_spl[0] + ip_spl[1] + ip_spl[2] +ip_spl[3]
-         if ip_int == "255255255255":
-            print('local broadcast')
-         else:
-            if int((ip_spl[0]),10) < 223:
-               print('unicast')
-            elif  int((ip_spl[0]),10) < 239:
-               print('multicast')
+flag = False
+while not flag:
+    try:
+        ip_spl = ip.split(".")
+       #делаем строку из ip:
+        ip_int = ip_spl[0] + ip_spl[1] + ip_spl[2] +ip_spl[3] 
+       #проверка на присутствие букв в адресе:
+        int((ip_spl[0]),10)/int((ip_spl[1]),10)/int((ip_spl[2]),10)/int((ip_spl[3]),10) 
+       #проверка на наличие разделителя "."
+        if '.' in ip:
+          #проверка на значение октетов меньше 255
+            if int((ip_spl[0]),10) <= 255 and int((ip_spl[1]),10) <= 255 and int((ip_spl[2]),10) <= 255 and int((ip_spl[3]),10) <= 255: 
+                flag = True
+                if ip_int == "255255255255":
+                    print('local broadcast')
+                else:
+                    if int((ip_spl[0]),10) < 223:
+                       print('unicast')
+                    elif  int((ip_spl[0]),10) < 239:
+                       print('multicast')
+                    else:
+                       print ('unused')      
             else:
-               print ('unused')
-      else:
-         print('Неправильный IP-адрес')
-   else:
-      print('Неправильный IP-адрес')
-except ZeroDivisionError:
-   if ip_int == "0000":
+                print('Неправильный IP-адрес')
+                ip = input ("Введите IP устройства еще раз: ")
+        else:
+            print('Неправильный IP-адрес')
+            ip = input ("Введите IP устройства еще раз: ")
+    except ZeroDivisionError:
+        if ip_int == "0000":
             print('unassigned')
-except ValueError:
-    print('Неправильный IP-адрес')
-except IndexError:
-   print('Неправильный IP-адрес')
+    except ValueError:
+        print('Неправильный IP-адрес')
+        ip = input ("Введите IP устройства еще раз: ")
+    except IndexError:
+        print('Неправильный IP-адрес')
+        ip = input ("Введите IP устройства еще раз: ")
