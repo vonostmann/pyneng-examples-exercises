a6bd16e252b7405d1edd04eedd41af58359a1c7b
vonostmann <nostroukh@yahoo.com>
2022-03-23 13:30:21 +0100

    Task draft

--- exercises/06_control_structures/task_6_2b.py
+++ exercises/06_control_structures/task_6_2b.py
@@ -12,3 +12,35 @@
 
 Ограничение: Все задания надо выполнять используя только пройденные темы.
 """
+ip = input ("Введите IP устройства: ")
+try:
+   ip_spl = ip.split(".")
+   #делаем строку из ip:
+   ip_int = ip_spl[0] + ip_spl[1] + ip_spl[2] +ip_spl[3] 
+   #проверка на присутствие букв в адресе:
+   int((ip_spl[0]),10)/int((ip_spl[1]),10)/int((ip_spl[2]),10)/int((ip_spl[3]),10) 
+   #проверка на наличие разделителя "."
+   if '.' in ip:
+      #проверка на значение октетов меньше 255
+      if int((ip_spl[0]),10) <= 255 and int((ip_spl[1]),10) <= 255 and int((ip_spl[2]),10) <= 255 and int((ip_spl[3]),10) <= 255: 
+         #ip_int = ip_spl[0] + ip_spl[1] + ip_spl[2] +ip_spl[3]
+         if ip_int == "255255255255":
+            print('local broadcast')
+         else:
+            if int((ip_spl[0]),10) < 223:
+               print('unicast')
+            elif  int((ip_spl[0]),10) < 239:
+               print('multicast')
+            else:
+               print ('unused')
+      else:
+         print('Неправильный IP-адрес')
+   else:
+      print('Неправильный IP-адрес')
+except ZeroDivisionError:
+   if ip_int == "0000":
+            print('unassigned')
+except ValueError:
+    print('Неправильный IP-адрес')
+except IndexError:
+   print('Неправильный IP-адрес')
