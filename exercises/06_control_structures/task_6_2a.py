820d2483c448eac2b862daad0a10e32c010c72c8
vonostmann <nostroukh@yahoo.com>
2022-03-23 12:58:46 +0100

    Task is solved. Tested on full ranfe of IPs

--- exercises/06_control_structures/task_6_2a.py
+++ exercises/06_control_structures/task_6_2a.py
@@ -12,22 +12,40 @@
 
 Если адрес задан неправильно, выводить сообщение: 'Неправильный IP-адрес'
 
-Сообщение "Неправильный IP-адрес" должно выводиться только один раз,
+Сообщение "Неправильный IP-адрес" должно выводиться только один раз,00
 даже если несколько пунктов выше не выполнены.
 
 Ограничение: Все задания надо выполнять используя только пройденные темы.
 """
 ip = input ("Введите IP устройства: ")
-ip_spl = ip.split(".")
-ip_int = ip_spl[0] + ip_spl[1] + ip_spl[2] +ip_spl[3]
-if ip_int == "0000":
-      print('unassigned')
-elif ip_int == "255255255255":
-      print('local broadcast')
-else:
-   if int((ip_spl[0]),10) < 223:
-      print('unicast')
-   elif  int((ip_spl[0]),10) < 239:
-      print('multicast')
+try:
+   ip_spl = ip.split(".")
+   #делаем строку из ip:
+   ip_int = ip_spl[0] + ip_spl[1] + ip_spl[2] +ip_spl[3] 
+   #проверка на присутствие букв в адресе:
+   int((ip_spl[0]),10)/int((ip_spl[1]),10)/int((ip_spl[2]),10)/int((ip_spl[3]),10) 
+   #проверка на наличие разделителя "."
+   if '.' in ip:
+      #проверка на значение октетов меньше 255
+      if int((ip_spl[0]),10) <= 255 and int((ip_spl[1]),10) <= 255 and int((ip_spl[2]),10) <= 255 and int((ip_spl[3]),10) <= 255: 
+         #ip_int = ip_spl[0] + ip_spl[1] + ip_spl[2] +ip_spl[3]
+         if ip_int == "255255255255":
+            print('local broadcast')
+         else:
+            if int((ip_spl[0]),10) < 223:
+               print('unicast')
+            elif  int((ip_spl[0]),10) < 239:
+               print('multicast')
+            else:
+               print ('unused')
+      else:
+         print('Неправильный IP-адрес')
    else:
-      print ('unused')
+      print('Неправильный IP-адрес')
+except ZeroDivisionError:
+   if ip_int == "0000":
+            print('unassigned')
+except ValueError:
+    print('Неправильный IP-адрес')
+except IndexError:
+   print('Неправильный IP-адрес')
